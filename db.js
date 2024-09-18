9ccdb8a4931c09c5afeb8b142069a422c49a623f
Ryan Nicolau Lopes <61668167+ryannlopes@users.noreply.github.com>
2024-09-18 19:58:58 -0300

    Atualizando o api para hospedagem

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 
-// Configurar a conexão ao banco de dados MySQL, usando variáveis de ambiente
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST || 'localhost', // Render vai definir essa variável
-  user: process.env.MYSQL_USER || 'root',      // Render vai definir essa variável
-  password: process.env.MYSQL_PASSWORD || '',  // Render vai definir essa variável
-  database: process.env.MYSQL_DB || 'sage'     // Render vai definir essa variável
-});
+// Usando a URL de conexão diretamente
+const db = mysql.createConnection('mysql://root:PDxSIcSuLnRDNCyNkoAcITJKrFMXWlXU@autorack.proxy.rlwy.net:27279/railway');
 
 db.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao banco de dados:', err.stack);
     return;
   }
-  console.log('Conectado ao banco de dados como id ' + db.threadId);
+  console.log('Conectado ao banco de dados com id ' + db.threadId);
 });
 
 module.exports = db;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const updateRoutes = require('./routes/updateRoutes');
 const deleteRoutes = require('./routes/deleteRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 27279;
 
 app.use(express.json());
 
